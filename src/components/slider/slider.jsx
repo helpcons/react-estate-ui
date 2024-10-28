7130548ff91140e9927cdf7d92270e3d81e98f5e
Helder Pereira <helder_leg@hotmail.com>
2024-10-27 21:12:56 -0300

    .

---
+++ src/components/slider/slider.jsx
@@ -0,0 +1,7 @@
+function Slider () {
+    return (
+        <div>nada de nada</div>
+    )
+}
+
+export default Slider
--- src/routes/singlePage/singlePage.jsx
+++ src/routes/singlePage/singlePage.jsx
@@ -1,7 +1,32 @@
+import Slider from '../../components/slider/slider'
+import { singlePostData } from '../../lib/dummydata'
+import './singlePage.scss'
+
 function SinglePage () {
     return (
-        <div>
-            <h1>Single Page</h1>
+        <div className="singlePage">
+            <div className="details">
+                <div className="wrapper">
+                    <Slider images={singlePostData.images}/>
+                    <div className="info">
+                        <div className="top">
+                            <div className="post">
+                                <h1>{singlePostData.title}</h1>
+                                <div className="address">
+                                    <img src="/pin.png" alt="" />
+                                    <span>{singlePostData.address}</span>
+                                </div>
+                                <div className="price">$ {singlePostData.price}</div>
+                            </div>
+                            <div className="user"></div>
+                        </div>
+                        <div className="bottom"></div>
+                    </div>
+                </div>
+            </div>
+            <div className="features">
+                <div className="wrapper"></div>
+            </div>
         </div>
     )
 }
